4e35a301bc7fa94c24c421e2a1b1ebae18c910a1
Colleen Bove <colleen@Colleens-MacBook-Pro.local>
2021-01-14 22:35:25 -0500

    snarky nat 20 comment

--- DnD_dice_roll.py
+++ DnD_dice_roll.py
@@ -6,6 +6,7 @@ dice_string = input('Enter the dice roll: ')
 roll_list = dice_string.split('d')
 dice_split = roll_list[1].split('+')
 dice_rep = roll_list[0]
+dice_rep = int(dice_rep)
 
 
 if len(dice_split) > 1:
@@ -15,12 +16,24 @@ if len(dice_split) > 1:
 dice_type = dice_split[0]
 dice_num = dice_type.replace('d', '')
 dice_int = int(dice_num)
-roll = random.randint(1, dice_int)
 
+roll_total = 0
+
+natural = False
+
+for i in range(dice_rep):
+    roll = random.randint(1, dice_int)
+    if dice_type == '20' and roll == 20:
+        natural = True
+
+    roll_total += roll
 
 if len(dice_split) > 1:
-    roll = roll + roll_modifier
+    roll_total = roll_total + roll_modifier
 
-print(f'Your awesome roll is: {roll}! (with {num_dice} die/dice)')
+dice = 'die'
 
+if dice_rep > 1:
+    dice = 'dice'
 
+print(f'{"OMG, you got a nat 20!" if natural else ""} Your awesome roll is {roll_total} (with {dice_rep} {dice})! Nicely done friend.')
